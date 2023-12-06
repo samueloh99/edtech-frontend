218e0d6e1d5caf4c0ddc15d1a4e0dc07d58ed2d3
samueloh99 <samueloh99@gmail.com>
2023-12-06 09:49:26 -0300

    Added a bg and height for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <div className="flex flex-row justify-between items-center main-container">
+    <div className="flex flex-row justify-between items-center main-container h-[70px] bg-white">
       <h1 className="text-xl font-bold">Education Platform</h1>
       <ul className="flex flex-row justify-center items-center gap-5 text-lg">
         <li>Pricing</li>
