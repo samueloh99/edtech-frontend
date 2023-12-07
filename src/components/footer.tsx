b0645892d7b09a8e2750c42534b421f2169d417f
samueloh99 <samueloh99@gmail.com>
2023-12-06 21:24:58 -0300

    Fixed header and footer details

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,33 +3,33 @@ export function Footer() {
     <div className="flex flex-col bg-white mt-60 w-full h-full md:h-[300px]">
       <div className="main-container flex flex-col w-full h-full justify-between items-start py-5 gap-10 md:gap-0">
         <div className="flex flex-col md:flex-row justify-between items-star w-full gap-10 md:gap-0">
-          <div className="flex flex-col justify-start items-start w-full gap-2">
+          <div className="flex flex-col justify-start items-center md:items-start w-full gap-2">
             <p className="text-xl font-bold">Education Platform</p>
-            <ul className="flex flex-col items-start text-md">
+            <ul className="flex flex-col items-center md:items-start text-md">
               <li>Home</li>
               <li>Plans & Pricing</li>
             </ul>
           </div>
-          <div className="flex flex-col md:flex-row w-full justify-between gap-5 md:gap-0">
-            <div className="flex flex-col justify-start items-start md:items-end gap-2">
+          <div className="flex flex-col md:flex-row w-full justify-between gap-10 md:gap-0">
+            <div className="flex flex-col justify-start items-center md:items-start w-full gap-2">
               <p className="text-lg font-bold">Community</p>
-              <ul className="flex flex-col md:items-end items-start text-md">
+              <ul className="flex flex-col items-center md:items-start text-md">
                 <li>Explore the community</li>
                 <li>Youtube</li>
                 <li>Newsletter</li>
               </ul>
             </div>
-            <div className="flex flex-col justify-start items-start md:items-end gap-2">
+            <div className="flex flex-col justify-start items-center md:items-start w-full gap-2">
               <p className="text-lg font-bold">Courses</p>
-              <ul className="flex flex-col md:items-end items-start text-md">
+              <ul className="flex flex-col items-center md:items-start text-md">
                 <li>All courses</li>
                 <li>Free courses</li>
                 <li>Pro courses</li>
               </ul>
             </div>
-            <div className="flex flex-col justify-start items-start md:items-end gap-2">
+            <div className="flex flex-col justify-start items-center md:items-start w-full gap-2">
               <p className="text-lg font-bold">Company</p>
-              <ul className="flex flex-col md:items-end items-start text-md">
+              <ul className="flex flex-col items-center md:items-start text-md">
                 <li>About us</li>
                 <li>Terms</li>
                 <li>Privacy</li>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
+
 export function Header() {
   return (
-    <div className="flex flex-row justify-between items-center main-container h-[70px] bg-white">
-      <h1 className="text-xl font-bold">Education Platform</h1>
-      <ul className="flex flex-row justify-center items-center gap-5 text-lg">
-        <li>Pricing</li>
-        <li>Log in</li>
-      </ul>
+    <div className="flex h-[70px] bg-white">
+      <div className="flex flex-row justify-between items-center main-container">
+        <Link href={"/"} className="text-xl font-bold">
+          Education Platform
+        </Link>
+        <ul className="flex flex-row justify-center items-center gap-5 text-lg">
+          <li>Pricing</li>
+          <li>Log in</li>
+        </ul>
+      </div>
     </div>
   );
 }
